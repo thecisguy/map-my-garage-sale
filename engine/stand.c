bc2b7ea5c0ca85d400e8b5a08f844ac656afe4b2
Blake Lowe <b.f.lowe@csuohio.edu>
2014-09-02 02:40:08 -0400

    added preliminary constructor

--- engine/stand.c
+++ engine/stand.c
@@ -1,5 +1,6 @@
 #include "grid.h"
 #include <stdbool.h>
+#include <stdlib.h>
 
 struct stand {
 	grid g;
@@ -11,4 +12,24 @@ struct stand {
 	double alpha;
 }
 
+stand new_stand(double red, double green, double blue, double alpha) {
+	stand ns = malloc(sizeof(struct stand));
+	if (ns == NULL)
+		goto out_ns;
+
+	ns->red = red;
+	ns->green = green;
+	ns->blue = blue;
+	ns->alpha = alpha;
+
+	ns->g = NULL;
+
+out_ns:;
+       return ns;
+}
+
 bool apply(stand s, grid g);
+
+bool rotateCW(grid g);
+bool rotateCCW(grid g);
+bool mirror(grid g);
