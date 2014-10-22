6b677c8e845ddc65cdf6faaef7c5e7547ee565c7
Fran7118 <jpolaniec@gmail.com>
2014-10-21 23:15:51 -0400

    Refactoring and changes to images.  Images are now Embedded Resources instead of copied content.
    Signed-off-by: Fran7118 <jpolaniec@gmail.com>

--- Frontend/OperationWidget.cs
+++ Frontend/OperationWidget.cs
@@ -1,35 +1,53 @@
 using System;
+using System.Reflection;
+using System.IO;
 using Gtk;
 
 namespace Frontend
 {
-	public class OperationWidget
-	{
-		public OperationWidget ()
-		{
-		}
+    public class OperationWidget
+    {
+        #region Constructor
+        public OperationWidget()
+        {
+        }
+        #endregion
+        #region Private Methods
+        /// <summary>
+        /// Creates a VBox.
+        /// </summary>
+        /// <returns>The operation widget.</returns>
+        /// <param name="iconFilePath">Icon file path.</param>
+        /// <param name="labelText">Label text.</param>
+        public Widget CreateOperationWidget(string iconResourceName, string labelText)
+        {
+            VBox box = new VBox(false, 0);
 
-		/// <summary>
-		/// Creates a VBox.
-		/// </summary>
-		/// <returns>The operation widget.</returns>
-		/// <param name="iconFilePath">Icon file path.</param>
-		/// <param name="labelText">Label text.</param>
-		public static Widget CreateOperationWidget(string iconFilePath, string labelText)
-		{
-			VBox box = new VBox (false, 0);
+            Stream imageStream = getSpecifiedResourceStream(iconResourceName);
+            using (Gtk.Image image = new Gtk.Image (imageStream))
+            {
+                Label label = new Label(labelText);
 
-			Gtk.Image image = new Gtk.Image (iconFilePath);
-			Label label = new Label (labelText);
+                box.PackStart(image, false, false, 3);
+                box.PackStart(label, false, false, 3);
 
-			box.PackStart (image, false, false, 3);
-			box.PackStart (label, false, false, 3);
+                image.Show();
+                label.Show();
+            }
+            return box;
+        }
 
-			image.Show ();
-			label.Show ();
-
-			return box;
-		}
-	}
+        /// <summary>
+        /// Returns a stream from the passed in Resource name
+        /// </summary>
+        /// <param name="resourceName">Resource name.</param>
+        private Stream getSpecifiedResourceStream(string resourceName)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Stream stream = assembly.GetManifestResourceStream(resourceName);
+            return stream;
+        }
+        #endregion
+    }
 }
 
