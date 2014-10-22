4c1872b0f3fccdeb485bb22280819e8672b79ed0
Blake Lowe <b.f.lowe@csuohio.edu>
2014-10-22 00:26:38 -0400

    create save_n_load source files

---
+++ engine/save_n_load.h
@@ -0,0 +1,22 @@
+/* save_n_load.h
+ * 
+ * This file contains declarations for the functions responsible
+ * for saving and loading the program from a file.
+ * 
+ * Copyright (C) 2014 - Blake Lowe, Jordan Polaniec
+ *
+ * This file is part of Map My Garage Sale.
+ * 
+ * Map My Garage Sale is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Map My Garage Sale is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Map My Garage Sale. If not, see <http://www.gnu.org/licenses/>.
+ */
