82b9e7a98c21282890a1deeaf7ecadc8162cfd05
Fran7118 <jpolaniec@gmail.com>
2014-10-25 11:06:02 -0400

    - Added method to return a drawn shape (Cairo) in a VBox - Added licensing comment

--- Frontend/OperationWidget.cs
+++ Frontend/OperationWidget.cs
@@ -1,3 +1,25 @@
+/* OperationWidget.cs
+ *
+ * Contains functions for initializing Widget objects.
+ *
+ * Copyright (C) 2014 - Blake Lowe, Jordan Polaniec
+ *
+ * This file is part of Map My Garage Sale.
+ * 
+ * Map My Garage Sale is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Map My Garage Sale is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Map My Garage Sale. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 using System;
 using System.Reflection;
 using System.IO;
@@ -34,6 +56,19 @@ namespace Frontend
                 image.Show();
                 label.Show();
             }
+            return box;
+        }
+
+        /// <summary>
+        /// Creates a visual representation of the Stand object.  Does not yet pull anything from engine.
+        /// </summary>
+        /// <returns>The stand.</returns>
+        /// <param name="cairoGraphic">Cairo graphic.</param>
+        public VBox createStand(CairoGraphic cairoGraphic)
+        {
+            VBox box = new VBox(true, 0);
+            box.Add(cairoGraphic);
+            cairoGraphic.Show();
             return box;
         }
 
