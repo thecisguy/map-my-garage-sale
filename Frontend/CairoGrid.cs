affc1aea195f0dbf4d9027bf071ca59b4eb19f2c
Fran7118 <jpolaniec@gmail.com>
2014-11-22 19:02:13 -0500

    implemented methods for drawing physical grid lines, added DrawLines bool for toggling

--- Frontend/CairoGrid.cs
+++ Frontend/CairoGrid.cs
@@ -9,11 +9,13 @@ namespace Frontend
     {
         public uint Height { get; set; }
         public uint Width { get; set; }
-        public string BackdropPath { get; set;}
+        public string BackdropPath { get; set; }
         private Gdk.Window window;
+        public bool DrawLines = true;
 
         public CairoGrid()
-        {}
+        {
+        }
 
         #region Drawing Methods
 
@@ -40,16 +42,21 @@ namespace Frontend
                         Console.WriteLine("Unable to save a scaled copy of the backdrop image");
                     }
                         
-                        //paint the image and then the tiles
-                        using (ImageSurface surface = new ImageSurface("temp"))
+                    //paint the image and then the tiles
+                    using (ImageSurface surface = new ImageSurface("temp"))
+                    {
+                        using (Context context = Gdk.CairoHelper.Create(window))
                         {
-                            using (Context context = Gdk.CairoHelper.Create(window))
+                            context.SetSource(new SurfacePattern(surface));
+                            context.Paint();
+                            Draw(context);
+
+                            if (DrawLines)
                             {
-                                context.SetSource(new SurfacePattern(surface));
-                                context.Paint();
-                                Draw();
+                                DrawGridLines(context);
                             }
                         }
+                    }
                 }
             }
             else
@@ -58,9 +65,30 @@ namespace Frontend
             }
         }
 
+        /// <summary>
+        /// Used when user wants a backdrop.  Saves creating a new context for no reason.
+        /// </summary>
+        /// <param name="context">Context.</param>
+        private void Draw(Context context)
+        {
+            for (int countHeight = 0; countHeight < Height; countHeight++)
+            {
+                for (int countWidth = 0; countWidth < Width; countWidth++)
+                {
+                    DrawTile(context, new PointD(countWidth, countHeight));
+                }
+                context.ClosePath();
+                context.Stroke();
+            }   
+        }
+
+
+        /// <summary>
+        /// Overload for when user wants no backdrop.  
+        /// </summary>
         private void Draw()
         {
-            using (Context context = Gdk.CairoHelper.Create(window))
+            using(Context context = Gdk.CairoHelper.Create(window))
             {
                 for (int countHeight = 0; countHeight < Height; countHeight++)
                 {
@@ -68,10 +96,58 @@ namespace Frontend
                     {
                         DrawTile(context, new PointD(countWidth, countHeight));
                     }
+                    context.ClosePath();
+                    context.Stroke();
+                }   
+            }
+        }
+
+        /// <summary>
+        /// Draws actual grid lines on the mapping area
+        /// </summary>
+        private void DrawGridLines(Context context)
+        {
+            //vertical grid lines
+            for (int countHeight = 0; countHeight < Height; countHeight++)
+            {
+                for (int countWidth = 0; countWidth < Width; countWidth+= 50)
+                {
+                    DrawVerticalLine(context, new PointD(countWidth, countHeight));
                 }
                 context.ClosePath();
                 context.Stroke();
             }
+
+            //horizontal grid lines
+            for (int countWidth = 0; countWidth < Width; countWidth++)
+            {
+                for (int countHeight = 0; countHeight < Height; countHeight += 50)
+                {
+                    DrawHorizontalLine(context, new PointD(countWidth, countHeight));
+                }
+                context.ClosePath();
+                context.Stroke();
+            }
+        }
+
+        private void DrawVerticalLine(Context context, PointD point)
+        {
+            context.Antialias = Antialias.Default;
+            context.SetSourceRGBA(0.9, 0.5, 0.1, 0.9);
+            context.LineCap = LineCap.Square;
+            context.LineWidth = 0.3;
+            context.MoveTo(point.X, point.Y);
+            context.LineTo(point.X + 5, point.Y);
+        }
+
+        private void DrawHorizontalLine(Context context, PointD point)
+        {
+            context.Antialias = Antialias.Default;
+            context.SetSourceRGBA(0.9, 0.5, 0.1, 0.9);
+            context.LineCap = LineCap.Square;
+            context.LineWidth = 0.3;
+            context.MoveTo(point.X, point.Y);
+            context.LineTo(point.X, point.Y+5);
         }
 
         /// <summary>
@@ -82,7 +158,7 @@ namespace Frontend
         /// <param name="point">Point.</param>
         private void DrawTile(Context context, PointD point)
         {
-            Cairo.Color color = EngineAPI.getColorOfTile((uint) point.Y,(uint) point.X); //spoofing color no engine call yet
+            Cairo.Color color = new Cairo.Color(0, 0, 0, 0.1); //EngineAPI.getColorOfTile((uint)point.Y, (uint)point.X); //spoofing color no engine call yet
 
             context.Antialias = Antialias.None;
             context.SetSourceRGBA(color.R, color.G, color.B, color.A);
