9f71702e40e1872e5dcf21d3c3669c69e814bf99
Fran7118 <jpolaniec@gmail.com>
2014-11-26 15:38:30 -0500

    Stand contains business properties of a Stand object in memory

--- Frontend/Model/Stand.cs
+++ Frontend/Model/Stand.cs
@@ -1,4 +1,26 @@
-﻿using System;
+﻿/* Stand.cs
+ *
+ * Contains the business properties for a Stand object and is used for display in the NodeView (treeview).
+ *
+ * Copyright (C) 2014 - Blake Lowe, Jordan Polaniec
+ *
+ * This file is part of Map My Garage Sale.
+ * 
+ * Map My Garage Sale is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Map My Garage Sale is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Map My Garage Sale. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using Gdk;
 using Cairo;
 
