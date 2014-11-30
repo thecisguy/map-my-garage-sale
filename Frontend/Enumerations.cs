4062383ebb8f97f2ba4f82d9049a3635aae79cf2
Fran7118 <jpolaniec@gmail.com>
2014-11-29 22:18:57 -0500

    added enum flag for Backdrop changes

--- Frontend/Enumerations.cs
+++ Frontend/Enumerations.cs
@@ -8,7 +8,8 @@ namespace Frontend
         {
             InitialGridDraw,
             StandDraw,
-            ExistingStandRedraw
+            ExistingStandRedraw,
+            BackdropChangeDraw
         }
     }
 }
