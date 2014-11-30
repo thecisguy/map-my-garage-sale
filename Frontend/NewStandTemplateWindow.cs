a03253df9292bd309997bb680980e5a65a530216
Fran7118 <jpolaniec@gmail.com>
2014-11-29 22:16:49 -0500

    recreated new stand template window to not subclass Gtk.Dialog. - Consolidated UI and added a button for drawing the new stand shape

---
+++ Frontend/NewStandTemplateWindow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Frontend
+{
+    public partial class NewStandTemplateWindow : Gtk.Window
+    {
+        public NewStandTemplateWindow() :
+            base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+        }
+    }
+}
+
