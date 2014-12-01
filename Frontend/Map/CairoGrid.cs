3f71dca2a3f752b1ac39c333cc0c1e12ea304366
Fran7118 <jpolaniec@gmail.com>
2014-11-30 21:06:46 -0500

    modified alpha for tiles to be solid

--- Frontend/Map/CairoGrid.cs
+++ Frontend/Map/CairoGrid.cs
@@ -159,7 +159,7 @@ namespace Frontend.Map
             //Console.WriteLine(point.Y + ":" + point.X + ":" + color.R + ":" + color.G + ":" + color.B + ":" + color.A); 
 
             context.Antialias = Antialias.None;
-            context.SetSourceRGBA(color.R, color.G, color.B, 0.4);
+            context.SetSourceRGBA(color.R, color.G, color.B, 1);
             context.LineCap = LineCap.Round;
             context.MoveTo(point.X, point.Y);
             context.LineTo(point.X + 5, point.Y);
