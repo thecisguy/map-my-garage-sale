e7f10d17ade7928af2f6c8cce5f0378bcf7ec1bd
Fran7118 <jpolaniec@gmail.com>
2014-12-02 18:36:41 -0500

    fixed another bug with drawing the backdrop

--- Frontend/MainWindow.cs
+++ Frontend/MainWindow.cs
@@ -642,7 +642,10 @@ public partial class MainWindow: Gtk.Window
                 case (int)Enumerations.DrawType.StandDraw:
                     {
                         //CairoGrid.DrawTile(context, new PointD(k + args.Event.Region.Clipbox.Left, i + args.Event.Region.Clipbox.Top));
-                        CairoGrid.DrawBackdrop(context);
+                        if (CairoGrid.BackdropPath.Length > 0)
+                        {
+                            CairoGrid.DrawBackdrop(context);
+                        }
                         CairoGrid.DrawGrid(context);
                         break;
                     }
@@ -663,7 +666,10 @@ public partial class MainWindow: Gtk.Window
 //                                CairoGrid.DrawTile(context, new PointD(k + args.Event.Region.Clipbox.Left, i + args.Event.Region.Clipbox.Top));
 //                            }
 //                        }
-                        CairoGrid.DrawBackdrop(context);
+                        if (CairoGrid.BackdropPath.Length > 0)
+                        {
+                            CairoGrid.DrawBackdrop(context);
+                        }
                         CairoGrid.DrawGrid(context);
 
                         //just going to update statusbar for now
